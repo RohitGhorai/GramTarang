45491c04f93f37446e3ae3f807ed7813f5787a90
RohitGhorai <98656284+RohitGhorai@users.noreply.github.com>
2023-01-24 01:41:54 +0530

    Initial commit

--- src/Pages/tp_pia/Tp_regn_process_2.js
+++ src/Pages/tp_pia/Tp_regn_process_2.js
@@ -1,201 +1,4 @@
-// import React from "react";
-
-// const Form = () => {
-//   const SO_Detail = [
-//     { title: "Address", type: "text" },
-//     { title: "District", type: "text" },
-//     { title: "City", type: "text" },
-//     { title: "State/UT", type: "text" },
-//     { title: "Pin code", type: "number" },
-//     { title: "Telephone", type: "number" },
-//     { title: "Mobile", type: "number" },
-//     { title: "Email Id", type: "email" },
-//     { title: "GST", type: "number" },
-//   ];
-
-//   return (
-//     <>
-//       {SO_Detail.map((col_1) => (
-//         <div className="row mb-3">
-//           <label for="colFormLabel" className="col-sm-2 col-form-label">
-//             {col_1.title}
-//           </label>
-//           <div className="col-sm-10">
-//             <input
-//               type={col_1.type}
-//               className="form-control"
-//               id="colFormLabel"
-//             />
-//           </div>
-//         </div>
-//       ))}
-//     </>
-//   );
-// };
-// const TpRegnProcess_2 = () => {
-//   const Scheme = [
-//     "DDUGKY",
-//     "PMKK",
-//     "Sagarmala",
-//     "PMKVY",
-//     "Roshni",
-//     "NULM",
-//     "OSDA",
-//     "CORPORATES",
-//     "MORTH-RPL",
-//     "MMKVY",
-//     "Samarth",
-//     "PMKUV",
-//     "PMAKVY",
-//     "Gujrat",
-//     "SURYAMITRA",
-//     "VBKY",
-//     "MEPMA",
-//     "Standalone Training Institutes",
-//     "HIMMAYAT",
-//     "NAI MANZIL",
-//     "SEEKHO AUR KAMAO",
-//     "SEEDAP - AYS",
-//     "Uttar Pradesh Skill Development Mission",
-//   ];
-//   const Organization = [
-//     "Institute Setup By Govt.",
-//     "Institute Setup By Corporate",
-//     "Institute Setup By Trust / NGO / Society",
-//     "Corporates",
-//     "Standalone Training Institutes",
-//     "Others",
-//   ];
-
-//   return (
-//     <section id="step-2" className="form-step d-none">
-//       <h2 className="font-normal" style={{ color: "#00008B" }}>
-//         Scheme & Organization Detail
-//       </h2>
-//       <form>
-//         <div className="form-group">
-//           <label for="exampleFormControlSelect2">Scheme</label>
-//           <select className="form-control">
-//             <option value="" selected="">
-//               --Select--
-//             </option>
-//             {Scheme.map((props) => (
-//               <option value={props}>{props}</option>
-//             ))}
-//           </select>
-//         </div>
-//         <div className="form-group" style={{ marginTop: "10px" }}>
-//           <label>Name of the Organization</label>
-//           <input
-//             className="form-control form-control-sm"
-//             type="text"
-//             placeholder="Enter Organization Name"
-//           />
-//         </div>
-//         <div className="form-group" style={{ marginTop: "10px" }}>
-//           <label for="exampleFormControlSelect2">
-//             Category of Organization
-//           </label>
-//           <select className="form-control">
-//             <option value="" selected="">
-//               --Select--
-//             </option>
-//             {Organization.map((props) => (
-//               <option value={props}>{props}</option>
-//             ))}
-//           </select>
-//         </div>
-//         <div className="form-group" style={{ marginTop: "10px" }}>
-//           <label>Affiliation / Accreditation</label>
-//           <input
-//             className="form-control form-control-sm"
-//             type="text"
-//             placeholder="Enter Organization Name"
-//           />
-//         </div>
-//         <div className="form-group" style={{ marginTop: "10px" }}>
-//           <label>Date of Incorporation</label>
-//           <input
-//             className="form-control form-control-sm"
-//             type="date"
-//             placeholder="dd/mm/yyyy"
-//           />
-//         </div>
-//       </form>
-//       <form className="row g-3">
-//         <div className="col-sm-6">
-//           <div className="form-group mt-5 mb-4">
-//             <h4 className="text-center" style={{ color: "#00008B" }}>
-//               Registered Office Address
-//             </h4>
-//           </div>
-
-//           <Form />
-//         </div>
-//         <div className="col-sm-6">
-//           <div className="form-group mt-5 mb-4">
-//             <h4 className="text-center" style={{ color: "#00008B" }}>
-//               Regional/State Office Address
-//             </h4>
-//           </div>
-
-//           <Form />
-//         </div>
-//         <div className="mt-4">
-//           <div className="row mb-3">
-//             <label for="inputEmail3" className="col-sm-2 col-form-label">
-//               Website
-//             </label>
-//             <div className="col-sm-10">
-//               <input type="email" className="form-control" id="inputEmail3" />
-//             </div>
-//           </div>
-//           <div className="row mb-3">
-//             <label for="inputEmail3" className="col-sm-2 col-form-label">
-//               PAN Card
-//             </label>
-//             <div className="col-sm-10">
-//               <input type="email" className="form-control" id="inputEmail3" />
-//             </div>
-//           </div>
-//           <div className="row mb-3">
-//             <label for="inputEmail3" className="col-sm-2 col-form-label">
-//               PAN Number
-//             </label>
-//             <div className="col-sm-10">
-//               <input type="number" className="form-control" id="inputEmail3" />
-//             </div>
-//           </div>
-//         </div>
-//       </form>
-
-//       <div className="mt-3 text-center">
-//         <button
-//           className="button btn-navigate-form-step"
-//           type="button"
-//           step_number="1"
-//         >
-//           Previous
-//         </button>
-
-//         <button
-//           className="button btn-navigate-form-step"
-//           type="button"
-//           step_number="3"
-//         >
-//           Next
-//         </button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default TpRegnProcess_2;
-
-
-import * as React from 'react';
-
-
+import * as React from "react";
 
 const Form = () => {
   const SO_Detail = [
@@ -262,105 +65,102 @@ const Organization = [
   "Standalone Training Institutes",
   "Others",
 ];
-export const TpRegnProcess_2 = <div className="container"> <section id="step-2" className="form-step">
-<h2 className="font-normal" style={{ color: "#00008B" }}>
-  Scheme & Organization Detail
-</h2>
-<form>
-  <div className="form-group">
-    <label for="exampleFormControlSelect2">Scheme</label>
-    <select className="form-control">
-      <option value="" selected="">
-        --Select--
-      </option>
-      {Scheme.map((props) => (
-        <option value={props}>{props}</option>
-      ))}
-    </select>
-  </div>
-  <div className="form-group" style={{ marginTop: "10px" }}>
-    <label>Name of the Organization</label>
-    <input
-      className="form-control form-control-sm"
-      type="text"
-      placeholder="Enter Organization Name"
-    />
-  </div>
-  <div className="form-group" style={{ marginTop: "10px" }}>
-    <label for="exampleFormControlSelect2">
-      Category of Organization
-    </label>
-    <select className="form-control">
-      <option value="" selected="">
-        --Select--
-      </option>
-      {Organization.map((props) => (
-        <option value={props}>{props}</option>
-      ))}
-    </select>
-  </div>
-  <div className="form-group" style={{ marginTop: "10px" }}>
-    <label>Affiliation / Accreditation</label>
-    <input
-      className="form-control form-control-sm"
-      type="text"
-      placeholder="Enter Organization Name"
-    />
-  </div>
-  <div className="form-group" style={{ marginTop: "10px" }}>
-    <label>Date of Incorporation</label>
-    <input
-      className="form-control form-control-sm"
-      type="date"
-      placeholder="dd/mm/yyyy"
-    />
-  </div>
-</form>
-<form className="row g-3">
-  <div className="col-sm-6">
-    <div className="form-group mt-5 mb-4">
-      <h4 className="text-center" style={{ color: "#00008B" }}>
-        Registered Office Address
-      </h4>
-    </div>
+export const TpRegnProcess_2 = (
+  <div className="container">
+    <section id="step-2" className="form-step">
+      <h2 className="font-normal" style={{ color: "#00008B" }}>
+        Scheme & Organization Detail
+      </h2>
+      <form className="row g-3 mt-3">
+        <div className="form-group">
+          <label for="exampleFormControlSelect2">Scheme</label>
+          <select className="form-control">
+            <option value="" selected="">
+              --Select--
+            </option>
+            {Scheme.map((props) => (
+              <option value={props}>{props}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group" style={{ marginTop: "10px" }}>
+          <label>Name of the Organization</label>
+          <input
+            className="form-control form-control-sm"
+            type="text"
+            placeholder="Enter Organization Name"
+          />
+        </div>
+        <div className="form-group" style={{ marginTop: "10px" }}>
+          <label for="exampleFormControlSelect2">
+            Category of Organization
+          </label>
+          <select className="form-control">
+            <option value="" selected="">
+              --Select--
+            </option>
+            {Organization.map((props) => (
+              <option value={props}>{props}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group" style={{ marginTop: "10px" }}>
+          <label>Affiliation / Accreditation</label>
+          <input
+            className="form-control form-control-sm"
+            type="text"
+            placeholder="Enter Organization Name"
+          />
+        </div>
+        <div className="form-group" style={{ marginTop: "10px" }}>
+          <label>Date of Incorporation</label>
+          <input
+            className="form-control form-control-sm"
+            type="date"
+            placeholder="dd/mm/yyyy"
+          />
+        </div>
+      </form>
+      <form className="row g-3">
+        <div className="col-sm-6">
+          <div className="form-group mt-5 mb-4">
+            <h4 className="text-center" style={{ color: "#00008B" }}>
+              Registered Office Address
+            </h4>
+          </div>
 
-    <Form />
-  </div>
-  <div className="col-sm-6">
-    <div className="form-group mt-5 mb-4">
-      <h4 className="text-center" style={{ color: "#00008B" }}>
-        Regional/State Office Address
-      </h4>
-    </div>
+          <Form />
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group mt-5 mb-4">
+            <h4 className="text-center" style={{ color: "#00008B" }}>
+              Regional/State Office Address
+            </h4>
+          </div>
 
-    <Form />
+          <Form />
+        </div>
+        <div className="mt-4">
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">Website</label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="inputEmail3" />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">PAN Card</label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="inputEmail3" />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">PAN Number</label>
+            <div className="col-sm-10">
+              <input type="number" className="form-control" id="inputEmail3" />
+            </div>
+          </div>
+        </div>
+      </form>
+    </section>
   </div>
-  <div className="mt-4">
-    <div className="row mb-3">
-      <label for="inputEmail3" className="col-sm-2 col-form-label">
-        Website
-      </label>
-      <div className="col-sm-10">
-        <input type="email" className="form-control" id="inputEmail3" />
-      </div>
-    </div>
-    <div className="row mb-3">
-      <label for="inputEmail3" className="col-sm-2 col-form-label">
-        PAN Card
-      </label>
-      <div className="col-sm-10">
-        <input type="email" className="form-control" id="inputEmail3" />
-      </div>
-    </div>
-    <div className="row mb-3">
-      <label for="inputEmail3" className="col-sm-2 col-form-label">
-        PAN Number
-      </label>
-      <div className="col-sm-10">
-        <input type="number" className="form-control" id="inputEmail3" />
-      </div>
-    </div>
-  </div>
-</form>
-</section>
-</div>;
+);
--- src/Pages/tp_pia/Tp_regn_process_4.js
+++ src/Pages/tp_pia/Tp_regn_process_4.js
@@ -1,62 +1,3 @@
-// import React from "react";
-
-// const TpRegnProcess_4 = () => {
-//   const PC_Detail = [
-//     { title: "Name", type: "text" },
-//     { title: "Designation", type: "text" },
-//     { title: "Resident Address", type: "text" },
-//     { title: "Permanent Address", type: "text" },
-//     { title: "Citizenship", type: "text" },
-//     { title: "Mobile Number", type: "number" },
-//     { title: "Alt. Mobile Number", type: "number" },
-//     { title: "Email Id", type: "email" },
-//     { title: "Alt. Email Id", type: "email" },
-//     { title: "User Name", type: "text" },
-//   ];
-//   return (
-//     <section id="step-4" className="form-step d-none">
-//       <h2 className="font-normal" style={{ color: "#00008B" }}>
-//         Project Contact Personal Details
-//       </h2>
-//       <h4 className="text-center mt-3" style={{ color: "#00008B" }}>
-//         Regional/State Office
-//       </h4>
-//       <form className="row g-3 mt-3">
-//         {PC_Detail.map((props) => (
-//           <div className="col-sm-6">
-//             <div className="row">
-//               <label for="colFormLabel" className="col-sm-2 col-form-label">
-//                 {props.title}
-//               </label>
-//               <div className="col-sm-10">
-//                 <input
-//                   type={props.type}
-//                   className="form-control"
-//                   id="colFormLabel"
-//                 />
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </form>
-//       <div className="text-center mt-3">
-//         <button
-//           className="button btn-navigate-form-step"
-//           type="button"
-//           step_number="3"
-//         >
-//           Previous
-//         </button>
-//         <button className="button submit-btn" type="submit">
-//           Submit & Pay Onboarding Fees
-//         </button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default TpRegnProcess_4;
-
 import * as React from "react";
 const PC_Detail = [
   { title: "Name", type: "text" },
@@ -70,7 +11,10 @@ const PC_Detail = [
   { title: "Alt. Email Id", type: "email" },
   { title: "User Name", type: "text" },
 ];
-export const TpRegnProcess_4 = <div className="container"> <section id="step-4" className="form-step">
+export const TpRegnProcess_4 = (
+  <div className="container">
+    {" "}
+    <section id="step-4" className="form-step">
       <h2 className="font-normal" style={{ color: "#00008B" }}>
         Project Contact Personal Details
       </h2>
@@ -96,4 +40,5 @@ export const TpRegnProcess_4 = <div className="container"> <section id="step-4" 
         ))}
       </form>
     </section>
-    </div>;
+  </div>
+);
